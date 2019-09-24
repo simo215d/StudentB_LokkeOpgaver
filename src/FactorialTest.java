9e00e9a9076d4628505eec959b9586cbfe11cb35
simo215d <simo215d@edu.easj.dk>
2019-09-24 14:54:17 +0200

    Initial commit

---
+++ src/FactorialTest.java
@@ -0,0 +1,11 @@
+import junit.framework.TestCase;
+
+public class FactorialTest extends TestCase {
+    public void testCalc(){
+        Factorial fact = new Factorial();
+        assertEquals(6, Factorial.calc(3));
+        assertEquals(-1, Factorial.calc(10000));
+        assertEquals(0, Factorial.calc(0));
+        assertEquals(-1, Factorial.calc(-4));
+    }
+}
